6b674edf8021604db17377e6282ef53d71338660
Mananchaya Khumtai <khumtai.mananchaya@moneyforward.co.jp>
2021-06-10 21:21:38 +0700

    Get difference between two dates, remove user_id in users/index.html.erb

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,4 +2,11 @@ class HomeController < ApplicationController
   def index
     @post_cont = Post.all
   end
+
+  def date_since_update(post_date)
+     diff_date = (post_date.to_date - DateTime.now.utc.to_date).to_i
+  end
+  helper_method :date_since_update
+
+
 end
