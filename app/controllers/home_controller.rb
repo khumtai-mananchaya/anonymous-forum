a1d7a0b2fdd9618106e11ec5e2b0bda9ff7e1f3c
Mananchaya Khumtai <khumtai.mananchaya@moneyforward.co.jp>
2021-06-11 07:06:57 +0700

    Add time_since_update, remove minus sign

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,12 +1,17 @@
 class HomeController < ApplicationController
   def index
-    @post_cont = Post.all
+    @post_cont = Post.all.order('created_at DESC')
   end
 
   def date_since_update(post_date)
-     diff_date = (post_date.to_date - DateTime.now.utc.to_date).to_i
+    diff_date = (post_date.to_date - DateTime.now.utc.to_date).to_i.abs
   end
   helper_method :date_since_update
+
+  def time_since_update(post_time)
+    diff_time = (Time.now.utc - post_time.to_time).to_i / 3600
+  end
+  helper_method :time_since_update
 
 
 end
