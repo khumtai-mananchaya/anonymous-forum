e0464bd6a952483bb4f65e3c1a3469e573d00bc1
Mananchaya Khumtai <khumtai.mananchaya@moneyforward.co.jp>
2021-06-14 14:26:26 +0700

    Complete rspec User testing

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,27 +5,42 @@ RSpec.describe User, type: :model do
     user = User.new(
        username: "TestingFoo",
        email:      "tester@example.com",
-       password:   "dottle-nouveau-pavilion-tights-furze",
+       password:   "randompasswordhere",
     )
-    expect(user).to be_valid
+    user.valid?
     end
 
   it "is invalid without an username" do
     user = User.new(username: nil)
-    expect(user).to_not be_valid
+    user.valid?
+    expect(user.errors[:username]).to include("can't be blank")
   end
 
   it "is invalid without an email" do
     user = User.new(email: nil)
-    expect(user).to_not be_valid
+    user.valid?
+    expect(user.errors[:email]).to include("can't be blank")
   end
 
   it "is invalid without a password" do
     user = User.new(password: nil)
-    expect(user).to_not be_valid
+    user.valid?
+    expect(user.errors[:password]).to include("can't be blank")
   end
 
-  it "is invalid with a duplicate email address"
+  it "is invalid with a duplicate email address" do
+    User.create(
+        username:  "testing_foo",
+        email:      "testing@gmail.com",
+        password:   "randompasswordhere",
+    )
+    user = User.new(
+        username:  "Tester",
+        email:      "testing@gmail.com",
+        password:   "dottle-nouveau-pavilion-tights-furze",
+    )
+    user.valid?
+    expect(user.errors[:email]).to include("has already been taken")
+  end
 
-  it "is invalid with wrong confirmation password"
 end
