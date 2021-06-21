e8c0f435f1607b565f00817d07e105e9a6307455
Mananchaya Khumtai <khumtai.mananchaya@moneyforward.co.jp>
2021-06-21 12:33:07 +0700

    Add validates to model, change invalidity check in rspec models, Keeping patch :update as it is for now

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,5 @@ class User < ApplicationRecord
   has_secure_password
   validates :email, presence: true, uniqueness: true, format: { with: /\A[^@\s]+@[^@\s]+\z/, message: 'Invalid email' }
   validates :username, presence: true, uniqueness: true
+  validates :password_digest, presence: true
 end
