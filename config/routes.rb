df2cd0773405b9cb107e3123ff119a8ce1146d3e
Mananchaya Khumtai <khumtai.mananchaya@moneyforward.co.jp>
2021-06-25 09:15:10 +0700

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -11,17 +11,18 @@ Rails.application.routes.draw do
   end
   root 'home#index'
   get 'index', to: 'home#index'
+
   get 'sign_up', to: 'registrations#new'
   post 'sign_up', to: 'registrations#create'
+
   get 'sign_in', to: 'sessions#new'
   post 'sign_in', to: 'sessions#create', as: 'log_in'
   delete 'logout', to: 'sessions#destroy'
+
   get 'password', to: 'passwords#edit', as: 'edit_password'
   patch 'password', to: 'passwords#update'
   get 'password/reset', to: 'password_resets#new'
   post 'password/reset', to: 'password_resets#create'
   get 'password/reset/edit', to: 'password_resets#edit'
   patch 'password/reset/edit', to: 'password_resets#update'
-  post 'posts/new', to: 'posts#create'
-  #patch 'posts/edit', to: 'posts#update'
 end
